aca90ec84d1f57018cd2736649ebf627586a154f
myrmica-habilis <michalportes1@gmail.com>
2019-12-14 00:35:10 +0100

    Add factory function for rules

--- plural.py
+++ plural.py
@@ -1,6 +1,16 @@
 import re
 
 
+def build_match_and_apply_functions(pattern, search, replace):
+    def matches_rule(word):
+        return re.search(pattern, word)
+
+    def apply_rule(word):
+        return re.sub(search, replace, word)
+
+    return matches_rule, apply_rule
+
+
 def match_sxz(noun):
     return re.search('[sxz]$', noun)
 
