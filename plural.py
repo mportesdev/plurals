a0fcfabf515419dc77f4edfd9db571e652bcd859
myrmica-habilis <michalportes1@gmail.com>
2019-12-13 23:08:12 +0100

    Add an iterable of rules, refactor plural function

--- plural.py
+++ plural.py
@@ -33,12 +33,14 @@ def apply_default(noun):
     return noun + 's'
 
 
+rules = ((match_sxz, apply_sxz),
+         (match_h, apply_h),
+         (match_y, apply_y),
+         (match_default, apply_default)
+         )
+
+
 def plural(noun):
-    if re.search('[sxz]$', noun):
-        return re.sub('$', 'es', noun)
-    elif re.search('[^aeioudgkprt]h$', noun):
-        return re.sub('$', 'es', noun)
-    elif re.search('[^aeiou]y$', noun):
-        return re.sub('y$', 'ies', noun)
-    else:
-        return noun + 's'
+    for matches_rule, apply_rule in rules:
+        if matches_rule(noun):
+            return apply_rule(noun)
