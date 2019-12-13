64197e9a9452301cdf4a1593414c42a7dc92f982
myrmica-habilis <michalportes1@gmail.com>
2019-12-13 23:02:38 +0100

    Add match and apply functions

--- plural.py
+++ plural.py
@@ -1,6 +1,38 @@
 import re
 
 
+def match_sxz(noun):
+    return re.search('[sxz]$', noun)
+
+
+def apply_sxz(noun):
+    return re.sub('$', 'es', noun)
+
+
+def match_h(noun):
+    return re.search('[^aeioudgkprt]h$', noun)
+
+
+def apply_h(noun):
+    return re.sub('$', 'es', noun)
+
+
+def match_y(noun):
+    return re.search('[^aeiou]y$', noun)
+
+
+def apply_y(noun):
+    return re.sub('y$', 'ies', noun)
+
+
+def match_default(noun):
+    return True
+
+
+def apply_default(noun):
+    return noun + 's'
+
+
 def plural(noun):
     if re.search('[sxz]$', noun):
         return re.sub('$', 'es', noun)
