6b9fcb4aa280f5f57c63a315270880fc217d1e4f
myrmica-habilis <michalportes1@gmail.com>
2019-12-14 00:50:20 +0100

    Add an iterable with patterns, create rules dynamically

--- plural.py
+++ plural.py
@@ -11,43 +11,13 @@ def build_match_and_apply_functions(pattern, search, replace):
     return matches_rule, apply_rule
 
 
-def match_sxz(noun):
-    return re.search('[sxz]$', noun)
+patterns = (('[sxz]$',           '$',  'es'),
+            ('[^aeioudgkprt]h$', '$',  'es'),
+            ('[^aeiou]y$',  'y$', 'ies'),
+            ('$',                '$',  's'))
 
-
-def apply_sxz(noun):
-    return re.sub('$', 'es', noun)
-
-
-def match_h(noun):
-    return re.search('[^aeioudgkprt]h$', noun)
-
-
-def apply_h(noun):
-    return re.sub('$', 'es', noun)
-
-
-def match_y(noun):
-    return re.search('[^aeiou]y$', noun)
-
-
-def apply_y(noun):
-    return re.sub('y$', 'ies', noun)
-
-
-def match_default(noun):
-    return True
-
-
-def apply_default(noun):
-    return noun + 's'
-
-
-rules = ((match_sxz, apply_sxz),
-         (match_h, apply_h),
-         (match_y, apply_y),
-         (match_default, apply_default)
-         )
+rules = [build_match_and_apply_functions(pattern, search, replace)
+         for (pattern, search, replace) in patterns]
 
 
 def plural(noun):
