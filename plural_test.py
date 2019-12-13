2215ca13fae49c03b7c6499f2dcffe914d2bd73a
myrmica-habilis <michalportes1@gmail.com>
2019-12-13 22:55:06 +0100

    Add more test inputs

--- plural_test.py
+++ plural_test.py
@@ -3,14 +3,15 @@ import pytest
 from plural import plural
 
 
-@pytest.mark.parametrize('input, expected',
-                         [
-                             ('bass', 'basses'),
-                             ('fax', 'faxes'),
-                             ('vacancy', 'vacancies'),
-                             ('boy', 'boys'),
-                             ('day', 'days'),
-                             ('agency', 'agencies'),
-                         ])
+@pytest.mark.parametrize('input, expected', [('bass', 'basses'),
+                                             ('fax', 'faxes'),
+                                             ('waltz', 'waltzes'),
+                                             ('coach', 'coaches'),
+                                             ('rash', 'rashes'),
+                                             ('cheetah', 'cheetahs'),
+                                             ('vacancy', 'vacancies'),
+                                             ('boy', 'boys'),
+                                             ('day', 'days'),
+                                             ('agency', 'agencies')])
 def test_plural(input, expected):
     assert plural(input) == expected
