b3774665ae3d4a504e2fb32cb822d01a6d738a9d
myrmica-habilis <michalportes1@gmail.com>
2019-12-19 12:52:04 +0100

    Add rule for nouns ending with f

--- README.rst
+++ README.rst
@@ -1,1 +1,3 @@
 Based on *Dive Into Python 3* by Mark Pilgrim, Chapter 6 *"Closures & Generators"* and Chapter 7 *"Classes & Iterators"*.
+
+Extended by some irregular English nouns.
--- plural_test.py
+++ plural_test.py
@@ -15,3 +15,11 @@ from plural import plural
                                                   ('agency', 'agencies')])
 def test_plural(input_noun, expected):
     assert plural(input_noun) == expected
+
+
+@pytest.mark.parametrize('input_noun, expected', [('leaf', 'leaves'),
+                                                  ('loaf', 'loaves'),
+                                                  ('dwarf', 'dwarves'),
+                                                  ('scarf', 'scarves')])
+def test_plural_irregular(input_noun, expected):
+    assert plural(input_noun) == expected
