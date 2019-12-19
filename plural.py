341e44b1b601a20111e1fe5df735e6eb923088fe
myrmica-habilis <michalportes1@gmail.com>
2019-12-19 11:05:51 +0100

    Add generator of rules

--- plural.py
+++ plural.py
@@ -11,15 +11,15 @@ def build_match_and_apply_functions(pattern, search, replace):
     return matches_rule, apply_rule
 
 
-rules = []
+def rules(rules_filename):
+    with open(rules_filename, encoding='utf-8') as pattern_file:
+        for line in pattern_file:
+            pattern, search, replace = line.split(None, 3)
+            yield build_match_and_apply_functions(pattern, search, replace)
 
-with open('plural-rules.txt', encoding='utf-8') as pattern_file:
-    for line in pattern_file:
-        pattern, search, replace = line.split(None, 3)
-        rules.append(build_match_and_apply_functions(pattern, search, replace))
 
-
-def plural(noun):
-    for matches_rule, apply_rule in rules:
+def plural(noun, rules_filename='plural-rules.txt'):
+    for matches_rule, apply_rule in rules(rules_filename):
         if matches_rule(noun):
             return apply_rule(noun)
+    raise ValueError('no matching rule for {0}'.format(noun))
