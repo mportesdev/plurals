46a7e076e66b8819efd67bb95977fb425efd0659
myrmica-habilis <michalportes1@gmail.com>
2019-12-19 12:13:29 +0100

    Fix naming (do not use a builtin name)

--- plural_test.py
+++ plural_test.py
@@ -3,15 +3,15 @@ import pytest
 from plural import plural
 
 
-@pytest.mark.parametrize('input, expected', [('bass', 'basses'),
-                                             ('fax', 'faxes'),
-                                             ('waltz', 'waltzes'),
-                                             ('coach', 'coaches'),
-                                             ('rash', 'rashes'),
-                                             ('cheetah', 'cheetahs'),
-                                             ('vacancy', 'vacancies'),
-                                             ('boy', 'boys'),
-                                             ('day', 'days'),
-                                             ('agency', 'agencies')])
-def test_plural(input, expected):
-    assert plural(input) == expected
+@pytest.mark.parametrize('input_noun, expected', [('bass', 'basses'),
+                                                  ('fax', 'faxes'),
+                                                  ('waltz', 'waltzes'),
+                                                  ('coach', 'coaches'),
+                                                  ('rash', 'rashes'),
+                                                  ('cheetah', 'cheetahs'),
+                                                  ('vacancy', 'vacancies'),
+                                                  ('boy', 'boys'),
+                                                  ('day', 'days'),
+                                                  ('agency', 'agencies')])
+def test_plural(input_noun, expected):
+    assert plural(input_noun) == expected
