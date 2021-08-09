32f436d09de7e0c592d3e8cffed22043f184840c
Michal Porteš <michalportes1@gmail.com>
2021-08-09 13:07:30 +0200

    Update tests

--- plural_test.py
+++ plural_test.py
@@ -3,23 +3,34 @@ import pytest
 from plural import plural
 
 
-@pytest.mark.parametrize('input_noun, expected', [('bass', 'basses'),
-                                                  ('fax', 'faxes'),
-                                                  ('waltz', 'waltzes'),
-                                                  ('coach', 'coaches'),
-                                                  ('rash', 'rashes'),
-                                                  ('cheetah', 'cheetahs'),
-                                                  ('vacancy', 'vacancies'),
-                                                  ('boy', 'boys'),
-                                                  ('day', 'days'),
-                                                  ('agency', 'agencies')])
-def test_plural(input_noun, expected):
-    assert plural(input_noun) == expected
+@pytest.mark.parametrize(
+    'noun, expected',
+    [
+        ('bass', 'basses'),
+        ('fax', 'faxes'),
+        ('waltz', 'waltzes'),
+        ('coach', 'coaches'),
+        ('rash', 'rashes'),
+        ('cheetah', 'cheetahs'),
+        ('sigh', 'sighs'),
+        ('vacancy', 'vacancies'),
+        ('boy', 'boys'),
+        ('day', 'days'),
+        ('agency', 'agencies'),
+    ]
+)
+def test_plural(noun, expected):
+    assert plural(noun) == expected
 
 
-@pytest.mark.parametrize('input_noun, expected', [('leaf', 'leaves'),
-                                                  ('loaf', 'loaves'),
-                                                  ('dwarf', 'dwarves'),
-                                                  ('scarf', 'scarves')])
-def test_plural_irregular(input_noun, expected):
-    assert plural(input_noun) == expected
+@pytest.mark.parametrize(
+    'noun, expected',
+    [
+        ('leaf', 'leaves'),
+        ('loaf', 'loaves'),
+        ('dwarf', 'dwarves'),
+        ('scarf', 'scarves')
+    ]
+)
+def test_irregular_plural(noun, expected):
+    assert plural(noun) == expected
